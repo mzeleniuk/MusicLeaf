9e7ced0eefa45dbfe40d6b11c5d6f0f2b816d464
Michael Zelenyuk <zell.xp@gmail.com>
2018-06-08 17:54:33 +0300

    Add React Sound and player controls logic

--- app/containers/app.jsx
+++ app/containers/app.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Axios from "axios";
+import Sound from "react-sound";
 
 class AppContainer extends React.Component {
   constructor(props) {
@@ -8,7 +9,12 @@ class AppContainer extends React.Component {
     this.client_id = "2f98992c40b8edf17423d93bda2e04ab";
 
     this.state = {
-      track: { stream_url: "", title: "", artwork_url: "" }
+      track: { stream_url: "", title: "", artwork_url: "" },
+      playStatus: Sound.status.STOPPED,
+      elapsed: "00:00",
+      total: "00:00",
+      position: 0,
+      playFromPosition: 0
     }
   };
 
@@ -29,11 +35,48 @@ class AppContainer extends React.Component {
       .catch(function (error) {
         console.log(error);
       });
-  }
+  };
+
+  prepareUrl(url) {
+    return `${url}?client_id=${this.client_id}`;
+  };
+
+  handleSongPlaying(audio) {
+    this.setState({
+      elapsed: this.formatMilliseconds(audio.position),
+      total: this.formatMilliseconds(audio.duration),
+      position: audio.position / audio.duration
+    });
+  };
+
+  formatMilliseconds(milliseconds) {
+    const hours = Math.floor(milliseconds / 3600000);
+    milliseconds = milliseconds % 3600000;
+
+    const minutes = Math.floor(milliseconds / 60000);
+    milliseconds = milliseconds % 60000;
+
+    const seconds = Math.floor(milliseconds / 1000);
+    milliseconds = Math.floor(milliseconds % 1000);
+
+    return (minutes < 10 ? "0" : "") + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+  };
+
+  handleSongFinished() {
+    this.randomTrack();
+  };
 
   render () {
     return (
-      <div className="music-leaf"></div>
+      <div className="music-leaf">
+        <Sound
+          url={this.prepareUrl(this.state.track.stream_url)}
+          playStatus={this.state.playStatus}
+          onPlaying={this.handleSongPlaying.bind(this)}
+          playFromPosition={this.state.playFromPosition}
+          onFinishedPlaying={this.handleSongFinished.bind(this)}
+        />
+      </div>
     );
   };
 };
